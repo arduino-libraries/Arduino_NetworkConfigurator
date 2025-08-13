300a38ef70523815a0f21bac0ae74bb97a0c5c66
fabik111 <fabiomassimo.centonze@gmail.com>
2025-08-13 10:53:14 +0200

    add user confirmation

--- examples/utility/DeleteConfiguration/DeleteConfiguration.ino
+++ examples/utility/DeleteConfiguration/DeleteConfiguration.ino
@@ -25,10 +25,34 @@ SerialAgentClass SerialAgent;
 GenericConnectionHandler conMan;
 NetworkConfiguratorClass NetworkConfigurator(conMan);
 
+bool waitResponse() {
+  bool confirmation = false;
+  bool proceed = false;
+  while (confirmation == false) {
+    if (Serial.available()) {
+      char choice = Serial.read();
+      switch (choice) {
+        case 'y':
+        case 'Y':
+          confirmation = true;
+          proceed = true;
+          break;
+        case 'n':
+        case 'N':
+          confirmation = true;
+          proceed = false;
+          break;
+        default:
+          continue;
+      }
+    }
+  }
+  return proceed;
+}
+
 void setup() {
   /* Initialize serial debug port and wait up to 5 seconds for port to open */
   Serial.begin(9600);
-  for(unsigned long const serialBeginTime = millis(); !Serial && (millis() - serialBeginTime <= 5000); ) { }
 
   /* Set the debug message level:
   * - DBG_ERROR: Only show error messages
@@ -39,15 +63,24 @@ void setup() {
   */
   setDebugMessageLevel(DBG_INFO);
 
+  while (!Serial);
+
   /* Set the KVStore */
   NetworkConfigurator.setStorage(kvstore);
-  /* Wipe out the network configuration */
-  if (NetworkConfigurator.resetStoredConfiguration()) {
-    Serial.println("Network configuration reset successfully.");
-  } else {
-    Serial.println("Failed to reset network configuration.");
-  }
 
+  Serial.println("\nWARNING! Running the sketch the stored network configuration will be erased.");
+  Serial.println("Do you want to proceed? Y/[n]");
+
+  if (true == waitResponse()) {
+    /* Wipe out the network configuration */
+    if (NetworkConfigurator.resetStoredConfiguration()) {
+      Serial.println("Network configuration reset successfully.");
+    } else {
+      Serial.println("Failed to reset network configuration.");
+    }
+  } else {
+    Serial.println("Aborted.");
+  }
 }
 
 void loop() {
