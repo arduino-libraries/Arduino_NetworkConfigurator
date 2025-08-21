8fe245ec7a5ff248b562872ff0a9a9a3e5cb916a
fabik111 <fabiomassimo.centonze@gmail.com>
2025-08-21 14:51:27 +0200

    Release version 0.4.1

--- src/ANetworkConfigurator_Config.h
+++ src/ANetworkConfigurator_Config.h
@@ -7,7 +7,7 @@
 */
 #pragma once
 
-#define ANetworkConfigurator_LIB_VERSION "0.4.0"
+#define ANetworkConfigurator_LIB_VERSION "0.4.1"
 
 #if defined(ARDUINO_SAMD_MKRWIFI1010)
   #define NETWORK_CONFIGURATOR_COMPATIBLE 1
